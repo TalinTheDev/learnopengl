babab5bd93e740d4d08a165dc548c0e5ddbbfd3c
Talin Sharma <talinsharma.dev@gmail.com>
2025-07-16 20:14:43 +0000

    Window gets created with a color and escape key quitting!

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,17 @@
 // clang-format on
 #include <cstdio>
 
-int main() {
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    printf("Failed to initialize GLAD\n");
-    return -1;
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
   }
+}
+
+int main() {
 
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -23,5 +29,25 @@ int main() {
     return -1;
   }
   glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    printf("Failed to initialize GLAD\n");
+    return -1;
+  }
+  glViewport(0, 0, 800, 600);
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+
+  glfwTerminate();
   return 0;
 }
